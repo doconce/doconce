29e47f3a8491649f9e3588266f04cdf4360345af
Martin Heroux <heroux.martin@gmail.com>
2020-11-21 07:15:25 +1100

    Update doc/src/blog/clean.sh to fully clean dir after running make.sh

--- doc/src/blog/clean.sh
+++ doc/src/blog/clean.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # will remove pygmentized HTML files: doconce clean
 rm -rf automake_sphinx.py _d*.log tmp* demo.html demo.md demo.mwiki demo.rst sphinx-rootdir
+rm -rf *.rst *.log conf.py *.dlog *.html demo_*  _* Makefile *.bat .demo*
