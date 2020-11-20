f6d2659ff4a396e91ce11e4b1ae0ba687889603c
Martin Heroux <heroux.martin@gmail.com>
2020-11-21 07:09:22 +1100

    Update doc/src/blog/make.sh to touch conf.py to avoid error from sphinx

--- doc/src/blog/make.sh
+++ doc/src/blog/make.sh
@@ -21,6 +21,7 @@ doconce format mwiki demo
 # (recall that new shout wikis require files to be uploaded)
 # and test in http://en.wikibooks.org/wiki/Sandbox
 
+touch conf.py
 doconce sphinx_dir copyright=hpl title=demo demo
 python automake_sphinx.py
 # check out sphinx-rootdir/_build/html/demo.html
