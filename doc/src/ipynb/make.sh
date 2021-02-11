e54686193a63185bea6174fb7b62f9e44850a63d
Alessandro Marin <AlessandroMarin80@gmail.com>
2021-02-11 18:34:45 +0100

    fixed issue 91 ipynb

--- doc/src/ipynb/make.sh
+++ doc/src/ipynb/make.sh
@@ -4,9 +4,8 @@ doconce format ipynb example
 ipynb_figure=imgtag
 ipynb_movie=ipynb
 ipynb_admon=hrule
-nbv=3
 
-doconce format ipynb info --no_preprocess --ipynb_figure=${ipynb_figure} ipynb_figure=${ipynb_figure} --ipynb_movie=${ipynb_movie} ipynb_movie=${ipynb_movie} --ipynb_admon=${ipynb_admon} ipynb_admon=${ipynb_admon} --ipynb_version=$nbv --encoding=utf-8
+doconce format ipynb info --no_preprocess --ipynb_figure=${ipynb_figure} ipynb_figure=${ipynb_figure} --ipynb_movie=${ipynb_movie} ipynb_movie=${ipynb_movie} --ipynb_admon=${ipynb_admon} ipynb_admon=${ipynb_admon} --encoding=utf-8
 # Must fix instructions since doconce performs certain actions for
 # some of the code segments we demonstrate
 doconce subst '" +%matplotlib inline\\n",\n +" +\\n",\n +' '' info.ipynb
