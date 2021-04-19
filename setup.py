246953aaf779598b32f1c492a7caffe162a563e3
Alessandro Marin <AlessandroMarin80@gmail.com>
2021-04-19 13:21:55 +0200

    version 1.5.9 and testing signing commit with -S

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="DocOnce",
-    version="1.5.8",
+    version="1.5.9",
     author='Hans Petter Langtangen, Alessandro Marin',
     author_email="hpl@simula.no, Alessandro.Marin@fys.uio.no",
     maintainer = "Kristian Gregorius Hustad",
