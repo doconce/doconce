19b5bcd578c9a31d437dbd4d58039f5e57eecb6a
Alessandro Marin <AlessandroMarin80@gmail.com>
2021-06-02 10:31:30 +0200

    version 1.5.10 and rm some old files

--- lib/doconce/__init__.py
+++ lib/doconce/__init__.py
@@ -87,7 +87,7 @@ Both directories contain a make.sh file for creating various formats.
 '''
 from __future__ import absolute_import
 
-__version__ = '1.5.9'
+__version__ = '1.5.10'
 version = __version__
 __author__ = 'Hans Petter Langtangen', 'Alessandro Marin', 'Johannes H. Ring', 'Kristian Gregorius Hustad'
 author = __author__
